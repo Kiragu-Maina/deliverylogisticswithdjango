103f87373a44a8f0a6c96993e0488caea81cc085
JumaKenn <jumaliko711@gmail.com>
2023-06-28 09:39:08 +0300

    database config

--- logisticsappkenchic/settings.py
+++ logisticsappkenchic/settings.py
@@ -87,22 +87,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": os.environ["PGDATABASE"],
-#         "USER": os.environ["PGUSER"],
-#         "PASSWORD": os.environ["PGPASSWORD"],
-#         "HOST": os.environ["PGHOST"],
-#         "PORT": os.environ["PGPORT"],
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["PGDATABASE"],
+        "USER": os.environ["PGUSER"],
+        "PASSWORD": os.environ["PGPASSWORD"],
+        "HOST": os.environ["PGHOST"],
+        "PORT": os.environ["PGPORT"],
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 cloudinary.config(
     cloud_name="dzac7jcg9",
     api_key="298999595751521",
